fa3009948dc5a7730122d2e08872395d113198af
Robert J <robert.jackson@armor.com>
2017-08-10 18:58:44 -0500

    Added comments and removed depricated material

--- expiry.py
+++ expiry.py
@@ -1,53 +1,67 @@
 #!/usr/bin/env python
+# Written by: Robert J.
 
-# import base64
 import csv
 from datetime import datetime, timedelta
 import boto3
+import time
+
+#######################################
+### Main Function #####################
+#######################################
 
 def main():
     report = get_report()
     out = read_data(report)
     sns_push(out)
+
+    # print output for CLI execution
     print(out),
+
+    # return output for manually running lambda
     return out
+
+
+#######################################
+### Program Specific Functions ########
+#######################################
 
 def get_report():
     client = boto3.client('iam')
     client.generate_credential_report()
 
-    # time.sleep(2)
+    time.sleep(2)
 
     payload = client.get_credential_report()
-
-    '''
-    test_hash ="dXNlcixhcm4sdXNlcl9jcmVhdGlvbl90aW1lLHBhc3N3b3JkX2VuYWJsZWQscGFzc3dvcmRfbGFzdF91c2VkLHBhc3N3b3JkX2xhc3RfY2hhbmdlZCxwYXNzd29yZF9uZXh0X3JvdGF0aW9uLG1mYV9hY3RpdmUsYWNjZXNzX2tleV8xX2FjdGl2ZSxhY2Nlc3Nfa2V5XzFfbGFzdF9yb3RhdGVkLGFjY2Vzc19rZXlfMV9sYXN0X3VzZWRfZGF0ZSxhY2Nlc3Nfa2V5XzFfbGFzdF91c2VkX3JlZ2lvbixhY2Nlc3Nfa2V5XzFfbGFzdF91c2VkX3NlcnZpY2UsYWNjZXNzX2tleV8yX2FjdGl2ZSxhY2Nlc3Nfa2V5XzJfbGFzdF9yb3RhdGVkLGFjY2Vzc19rZXlfMl9sYXN0X3VzZWRfZGF0ZSxhY2Nlc3Nfa2V5XzJfbGFzdF91c2VkX3JlZ2lvbixhY2Nlc3Nfa2V5XzJfbGFzdF91c2VkX3NlcnZpY2UsY2VydF8xX2FjdGl2ZSxjZXJ0XzFfbGFzdF9yb3RhdGVkLGNlcnRfMl9hY3RpdmUsY2VydF8yX2xhc3Rfcm90YXRlZAo8cm9vdF9hY2NvdW50Pixhcm46YXdzOmlhbTo6OTc2MTY4Mjk1MjI4OnJvb3QsMjAxNi0wNy0yM1QwNzowNzo0OSswMDowMCxub3Rfc3VwcG9ydGVkLDIwMTctMDctMjBUMTU6MTg6MjkrMDA6MDAsbm90X3N1cHBvcnRlZCxub3Rfc3VwcG9ydGVkLHRydWUsZmFsc2UsTi9BLE4vQSxOL0EsTi9BLGZhbHNlLE4vQSxOL0EsTi9BLE4vQSxmYWxzZSxOL0EsZmFsc2UsTi9BCnJvYmVydC5qYWNrc29uLGFybjphd3M6aWFtOjo5NzYxNjgyOTUyMjg6dXNlci9yb2JlcnQuamFja3NvbiwyMDE3LTA3LTE4VDIxOjQxOjM2KzAwOjAwLHRydWUsMjAxNy0wOC0wN1QxNTo1ODoxNiswMDowMCwyMDE3LTA3LTE4VDIxOjU4OjE3KzAwOjAwLDIwMTctMDktMDFUMjE6NTg6MTcrMDA6MDAsdHJ1ZSxmYWxzZSxOL0EsTi9BLE4vQSxOL0EsZmFsc2UsTi9BLE4vQSxOL0EsTi9BLGZhbHNlLE4vQSxmYWxzZSxOL0EKcm9iZXJ0LmphY2tzb24uY2xpLGFybjphd3M6aWFtOjo5NzYxNjgyOTUyMjg6dXNlci9yb2JlcnQuamFja3Nvbi5jbGksMjAxNy0wNy0xOVQxMzozNDowMSswMDowMCxmYWxzZSxOL0EsTi9BLE4vQSxmYWxzZSx0cnVlLDIwMTctMDctMTlUMTM6MzQ6MDErMDA6MDAsMjAxNy0wOC0xMFQxNDoxNjowMCswMDowMCx1cy1lYXN0LTEsaWFtLGZhbHNlLE4vQSxOL0EsTi9BLE4vQSxmYWxzZSxOL0EsZmFsc2UsTi9B"
-    data = base64.b64decode(test_hash)
-    '''
 
     data = payload['Content']
 
     return data
 
+
 def read_data(data):
     value = ''
     for i in csv.DictReader(data.split()):
+        print(i['user'])
         if i['password_last_changed'] == 'not_supported':
             continue
         if i['password_last_changed'] == 'N/A':
             continue
 
+        # date information for parsing
         fmt         = '%Y-%m-%dT%H:%M:%S+00:00'
         date        = i['password_last_changed']
 
+        # date changed and now
         changed     = datetime.strptime(date, fmt)
         now         = datetime.now()
 
+        # date difference
         diff        = now - changed
         diff_days   = diff.total_seconds()/3600/24
         expiration  = changed + timedelta(45)
 
-        if diff_days >= 35:
+        if diff_days >= -1:
             value = value + \
                 '{}\'s password WILL expire at {}.'.format(
                     i['user'],
@@ -68,6 +82,7 @@ def read_data(data):
 
     return value
 
+
 def sns_push(out):
     client = boto3.client('sns')
     response = client.publish(
@@ -75,22 +90,18 @@ def sns_push(out):
         Message=out,
         Subject='Upcoming Password Expirations',
         MessageStructure='string'
-''',
-        MessageAttributes={
-            'string': {
-                'DataType': 'string',
-                'StringValue': 'string',
-                'BinaryValue': b'bytes'
-            }
-        }
-'''
     )
 
     return response
 
 
+#######################################
+### Execution #########################
+#######################################
+
 if __name__ == "__main__":
     main()
+
 
 def execute_me_lambda(event, context):
     out = main()
